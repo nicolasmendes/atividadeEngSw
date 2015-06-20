146ca8a7ddfad3d7ecd4d69da9b5fbca47e2b935
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 16:37:19 -0300

    versao 1.2

--- src/atividadeengsw/AtividadeEngSw.java
+++ src/atividadeengsw/AtividadeEngSw.java
@@ -5,9 +5,20 @@ public class AtividadeEngSw {
     
     private int A1;
     
+    private String Aramo4;
+
+    public String getAramo4() {
+        return Aramo4;
+    }
+
+    public void setAramo4(String Aramo4) {
+        this.Aramo4 = Aramo4;
+    }
+
     private float A2;
     
     private String A3;
+    
 
     public String getA3() {
         return A3;
