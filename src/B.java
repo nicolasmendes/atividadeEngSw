4c4c88768db551c0a34a77e64b9e276453ae2580
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 16:48:12 -0300

    versao 1.3

---
+++ src/B.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class B {
+    
+    private int B1;
+    
+    private float B2;
+    
+    private String B4;
+
+    public String getB4() {
+        return B4;
+    }
+
+    public void setB4(String B4) {
+        this.B4 = B4;
+    }
+
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+}
---
+++ src/atividadeengsw/A.java
@@ -0,0 +1,69 @@
+
+package atividadeengsw;
+
+public class A {
+    
+    private int A1;
+    
+    private String Aramo4;
+    private String Aprinc4;
+   
+
+    public String getAprinc4() {
+        return Aprinc4;
+    }
+
+    public void setAprinc4(String Aprinc4) {
+        this.Aprinc4 = Aprinc4;
+    }
+
+    
+
+    public String getAramo4() {
+        return Aramo4;
+    }
+
+    public void setAramo4(String Aramo4) {
+        this.Aramo4 = Aramo4;
+    }
+
+    private float A2;
+    
+    private String A3;
+    
+
+    public String getA3() {
+        return A3;
+    }
+
+    public void setA3(String A3) {
+        this.A3 = A3;
+    }
+
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    public static float soma(int a, float b) {
+        return a + b;
+    }
+    public void main(String[] args) {
+        A1 = 2;
+        A2 = 3;
+        float result = soma(A1, A2);
+    }
+    
+}
