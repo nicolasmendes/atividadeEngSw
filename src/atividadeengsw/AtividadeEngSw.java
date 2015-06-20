332c1475f1d168801d6ac4fbb9bc4627c9a96731
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 16:13:18 -0300

    versao 1.0

---
+++ src/atividadeengsw/AtividadeEngSw.java
@@ -0,0 +1,31 @@
+
+package atividadeengsw;
+
+public class AtividadeEngSw {
+    
+    private int A1;
+    
+    private float A2;
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+
+    public void main(String[] args) {
+       
+    }
+    
+}
